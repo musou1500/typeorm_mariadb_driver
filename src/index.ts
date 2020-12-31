9d1db27c7fd46497598faa65e9bdd20fa8132452
musou1500 <musou1500@gmail.com>
2020-12-31 11:05:04 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import {
+  Column,
+  createConnection,
+  Entity,
+  getManager,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity()
+class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ unique: true })
+  name!: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+(async () => {
+  const conn = await createConnection({
+    type: "mariadb",
+    username: "root",
+    password: "root",
+    host: "localhost",
+    port: 13306,
+    database: "app",
+    entities: [User],
+    synchronize: true,
+    logging: true,
+  });
+
+  await conn.createQueryBuilder().delete().from(User).execute();
+  const user1 = await getManager().save(User, new User("test"));
+  const users = [user1, new User("test")];
+  await getManager().transaction((transactional) =>
+    transactional
+      .getRepository(User)
+      .createQueryBuilder()
+      .insert()
+      .orUpdate({ conflict_target: ["name", "id"], overwrite: ["name", "id"] })
+      .values(users)
+      .updateEntity(true)
+      .execute()
+  );
+
+  console.log(users);
+
+  console.log(await getManager().find(User));
+})();
